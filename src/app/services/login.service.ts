8d9ed69f102bfc5a76787a6ab8faabf39479ef16
Jonathan Vargas <jonathan.vargas01@epn.edu.ec>
2023-06-20 08:55:20 -0500

    feat: login

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { find, map, Observable } from 'rxjs';
+import { Injectable, Injector } from '@angular/core';
+import { Router } from '@angular/router';
+import { map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user.interface';
 
@@ -9,14 +10,24 @@ import { User } from '../models/user.interface';
 })
 export class LoginService {
   private url = environment.apiUrl;
-  user!: User
-  constructor(private http: HttpClient) {}
+  user!: User;
+  private router!: Router;
+  constructor(private http: HttpClient, private injector: Injector) {
+    this.router = injector.get(Router);
+  }
 
   signInUser(data: any): Observable<User[] | null> {
-    return this.http.get<User[] | null>(
-      `${this.url}users?phone=${data.phone}&password=${data.password}`
-    ).pipe(
-      map((data: User[] | null) => data && data.length > 0 ? data : null ),
-    );
+    return this.http
+      .get<User[] | null>(
+        `${this.url}users?phone=${data.phone}&password=${data.password}`
+      )
+      .pipe(
+        map((data: User[] | null) => (data && data.length > 0 ? data : null))
+      );
+  }
+
+  logOut() {
+    localStorage.removeItem('user');
+    this.router.navigate(['/login']);
   }
 }
